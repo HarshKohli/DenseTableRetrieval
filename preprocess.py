ffbb0aeebdfbf17a61c1b48e7a6ec1c47e58bf02
HarshKohli <harsh14791@gmail.com>
2022-01-03 13:21:12 +0530

    variable name cleanup

--- preprocess.py
+++ preprocess.py
@@ -6,8 +6,8 @@ from utils.data_utils import parse_zipped, filter_useful_tables, write_datafile,
 
 config = yaml.safe_load(open('config.yml', 'r'))
 
-train_data, train_tables = parse_zipped(config['train_data_dir'])
-dev_data, dev_tables = parse_zipped(config['dev_data_dir'])
+train_data, train_tables = parse_zipped(config['pretrain_data_dir'])
+dev_data, dev_tables = parse_zipped(config['pretrain_dev_data_dir'])
 
 all_tables = set()
 for table in train_tables:
@@ -19,10 +19,11 @@ filtered_ids, filtered_tables = filter_useful_tables(config['wikitables'], all_t
 
 print('Starting negative sampling')
 train_data, dev_data = negative_sampling(train_data, dev_data, filtered_ids, filtered_tables,
-                                         config['random_neg_ratio'], config['hard_neg_ratio'], config['sample_hard'])
+                                         config['random_neg_ratio'], config['hard_neg_ratio'],
+                                         config['sample_hard_pretrain'])
 
 print('Writing preprocessed train data')
-write_datafile(train_data, config['train_data'], filtered_ids)
+write_datafile(train_data, config['pretrain_data'], filtered_ids)
 print('Writing preprocessed dev data')
-write_datafile(dev_data, config['dev_data'], filtered_ids)
+write_datafile(dev_data, config['pretrain_dev_data'], filtered_ids)
 print('Done preprocessing')
